527314d3a0dd779f0422e9fa8e514fcaeb81d6d8
Luis <garcialuisgonzalo@gmail.com>
2025-08-06 20:57:10 -0300

    tramites y servicios terminado

--- Pagina Policia Per/Controllers/TramitesController.cs
+++ Pagina Policia Per/Controllers/TramitesController.cs
@@ -10,38 +10,31 @@ namespace Pagina_Policia_Per.Controllers
         {
             new() { Id = 1, Titulo = "Certificado de Reincidencia", ImagenUrl = "/img/tramites/reincidencia.jpg", UrlDestino = "/Tramites/Reincidencia" },
             new() { Id = 2, Titulo = "Certificado de Buena Conducta", ImagenUrl = "/img/tramites/buena-conducta.jpg", UrlDestino = "/Tramites/BuenaConducta" },
-            
-            // CAMBIO AQUÍ: La UrlDestino ahora apunta a nuestra nueva página de armas.
             new() { Id = 3, Titulo = "Armas", ImagenUrl = "/img/tramites/armas.jpg", UrlDestino = "/Tramites/Armas" },
-
-            new() { Id = 4, Titulo = "Verificación Vehicular", ImagenUrl = "/img/tramites/verificacion.jpg", UrlDestino = "#" },
-            new() { Id = 5, Titulo = "Video Vigilancia Urbana", ImagenUrl = "/img/tramites/video-vigilancia.jpg", UrlDestino = "#" },
-            new() { Id = 6, Titulo = "Agencias y Vigilancia", ImagenUrl = "/img/tramites/agencias.jpg", UrlDestino = "#" },
-            new() { Id = 7, Titulo = "Transacciones Ganaderas", ImagenUrl = "/img/tramites/ganaderas.jpg", UrlDestino = "#" },
-            new() { Id = 8, Titulo = "Certificado de Vecindad y Supervivencia", ImagenUrl = "/img/tramites/vecindad.jpg", UrlDestino = "#" },
-            new() { Id = 9, Titulo = "Revenido Químico", ImagenUrl = "/img/tramites/revenido.jpg", UrlDestino = "#" },
-            new() { Id = 10, Titulo = "Rifas y Bonos", ImagenUrl = "/img/tramites/rifas.jpg", UrlDestino = "#" },
-            new() { Id = 11, Titulo = "División Policía Adicional", ImagenUrl = "/img/tramites/adicional.jpg", UrlDestino = "#" }
+            new() { Id = 4, Titulo = "Verificación Vehicular", ImagenUrl = "/img/tramites/verificacion.jpg", UrlDestino = "/Tramites/VerificacionVehicular" },
+            new() { Id = 5, Titulo = "Agencias y Vigilancia", ImagenUrl = "/img/tramites/agencias.jpg", UrlDestino = "/Tramites/AgenciasVigilancia" },
+            new() { Id = 6, Titulo = "Transacciones Ganaderas", ImagenUrl = "/img/tramites/ganaderas.jpg", UrlDestino = "/Tramites/TransaccionesGanaderas" },
+            new() { Id = 7, Titulo = "Certificado de Vecindad y Supervivencia", ImagenUrl = "/img/tramites/vecindad.jpg", UrlDestino = "/Tramites/VecindadSupervivencia" },
+            new() { Id = 8, Titulo = "Revenido Químico", ImagenUrl = "/img/tramites/revenido.jpg", UrlDestino = "/Tramites/RevenidoQuimico" },
+            new() { Id = 9, Titulo = "Rifas y Bonos", ImagenUrl = "/img/tramites/rifas.jpg", UrlDestino = "/Tramites/RifasBonos" },
+            
+            // CAMBIO AQUÍ: La UrlDestino ahora apunta a nuestra nueva página.
+            new() { Id = 10, Titulo = "División Policía Adicional", ImagenUrl = "/img/tramites/adicional.jpg", UrlDestino = "/Tramites/PoliciaAdicional" }
         };
 
-        public IActionResult Index()
-        {
-            return View(_tramites);
-        }
-
-        public IActionResult Reincidencia()
-        {
-            return View();
-        }
-
-        public IActionResult BuenaConducta()
-        {
-            return View();
-        }
+        public IActionResult Index() { return View(_tramites); }
+        public IActionResult Reincidencia() { return View(); }
+        public IActionResult BuenaConducta() { return View(); }
+        public IActionResult Armas() { return View(); }
+        public IActionResult VerificacionVehicular() { return View(); }
+        public IActionResult AgenciasVigilancia() { return View(); }
+        public IActionResult TransaccionesGanaderas() { return View(); }
+        public IActionResult VecindadSupervivencia() { return View(); }
+        public IActionResult RevenidoQuimico() { return View(); }
+        public IActionResult RifasBonos() { return View(); }
 
         // ACCIÓN NUEVA AÑADIDA:
-        // Responde a la URL /Tramites/Armas y muestra la nueva vista.
-        public IActionResult Armas()
+        public IActionResult PoliciaAdicional()
         {
             return View();
         }
